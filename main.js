4a9e621c29cddf55576d35c50a145cab363ab617
Ogohi <phoenix.cod4@mail.ru>
2023-03-11 13:46:22 +0500

    save

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+ var age = +prompt ('Введите свой возраст')
+
+
+ if (age > 0 && age < 18 ) {
+    alert ('Вам нужно учиться')
+    console.log('Вам нужно учиться');
+ }else if (age > 18 && age < 50 ) {
+    alert ('Вам нужно работать')
+    console.log('Вам нужно работать');
+ }else if (age > 50 && age < 59 ) {
+    alert ('Вам скоро на пенсию')
+    console.log('Вам скоро на пенсию');
+ }else if (age > 59 && age < 100 ) {
+    alert ('Вы пенсионер')
+    console.log('Вы пенсионер');
+ }else if (age > 100) {
+    alert ('Что то пошло не так')
+ }
+
+
+ 
+
+ 
+
+
